2c0dcc382b48c21338b8c515023e8562358ead94
LucianoEscalona <luci.escalona@duocuc.cl>
2025-10-24 12:20:51 -0300

    terminando el detalle de boleta

--- src/components/CardBoletas.jsx
+++ src/components/CardBoletas.jsx
@@ -1,9 +1,57 @@
 import "../styles/cssESCALONA.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function CardBoletas(boleta){
+function CardBoletas({brp, prods, style}){
+    let [ano,mes,dia] = brp.comprador.fecha_compra.split("-")
+    var ndia = dia.substring(0,2)
+    var total = 0
+    prods.forEach(p=>{
+        total += p.cantidad * p.precio
+    })
     return(
-        <div className="base_tarjeta">
-            <strong>BOLETA N°-{boleta.n_boleta}</strong>
+        <div className="base_tarjeta blt-tarjeta">
+            <div className="blt-bordes">
+                <div className="blt-bordes mt-1 mb-1 p-2 blt-jst">
+                    <strong>BOLETA N° - {brp.n_boleta}</strong>
+                </div>
+            </div>
+            <div className="p-2">
+                <div className="blt-jst mb-2 mt-1">
+                    <strong>Pasteleria mil sabores</strong>
+                </div>
+                <div className="row g-0">
+                    <p className="mb-1 col-6">Direccion:</p>
+                    <p className="mb-1 col-6 blt-mandar-end">Una calle de Santiago no se</p>
+                </div>
+                <div className="row g-0">
+                    <p className="mb-1 col-6">Fecha:</p>
+                    <p className="mb-1 col-6 blt-mandar-end">{ano}-{mes}-{ndia}</p>
+                </div>
+            </div>
+            <div className="blt-bordes pt-3 pb-2">
+                <div className="row g-0">
+                    <strong className="col-6">Nombre</strong>
+                    <strong className="col-2 blt-mandar-end">Unids</strong>
+                    <strong className="col-4 blt-mandar-end">Precio</strong>
+                </div>
+                <div>
+                    {
+                        prods.map((p,i)=>(
+                            <div key={i} className={style}>
+                                <p className="mb-1 col-6">{p.nombre}</p>
+                                <p className="mb-1 col-2 blt-mandar-end">x{p.cantidad}</p>
+                                <p className="mb-1 col-4 blt-mandar-end">${p.cantidad * p.precio}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="blt-espaciado mt-3">
+                <div className="row g-0 fondo">
+                    <strong className="mb-0 col-6">TOTAL</strong>
+                    <p className="mb-0 col-6 blt-mandar-end">${total}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/DetalleBoleta.jsx
+++ src/pages/DetalleBoleta.jsx
@@ -1,31 +1,59 @@
 import { useEffect, useState } from "react"
 import CardBoletas from "../components/CardBoletas"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import "../styles/cssESCALONA.css"
 
 function DetalleBoleta(){
 
     const [lsBoletas, setLsBoletas] = useState([])
-    const [codigo, setCodigo] = useState([])
-    const [recboleta, setRecboleta] = useState([])
+    const [recboleta, setRecboleta] = useState(null)
+    const [productos, setProductos] = useState(null)
+    const [estilo, setEstilo] = useState("")
 
     useEffect(()=>{
-        setLsBoletas(JSON.parse(localStorage.getItem("historial_boletas")))
-        setCodigo(JSON.parse(localStorage.getItem("BOLETA_SELECTED")))
-        lsBoletas.forEach(boleta=>{
-            if(boleta.n_boleta == String(codigo)){
-                console.log(boleta)
-                setRecboleta(boleta)
-            }
+
+        const pg = JSON.parse(localStorage.getItem("productosAdmin")) || []
+        const ls = JSON.parse(localStorage.getItem("historial_boletas")) || []
+        setLsBoletas(ls)
+
+        var codigo = JSON.parse(localStorage.getItem("BOLETA_SELECTED"))
+        
+        const encontrarBoleta = ls.find(boleta => boleta.n_boleta == codigo)
+        if (encontrarBoleta){setRecboleta(encontrarBoleta)}
+        
+        const prods = encontrarBoleta.productos_comprados
+        const listp = []
+
+        var est = " row g-0"
+        
+        prods.forEach(p=>{
+            pg.forEach(pg=>{
+                if(pg.prod_codigo == p.codigo_producto){
+                    const producto_info = {
+                        nombre: pg.nombre,
+                        cantidad: parseInt(p.cantidad_producto),
+                        precio: parseInt(pg.precio)
+                    }
+                    if(est == "fondo row g-0"){est = " row g-0"}else{est = "fondo row g-0"}
+                    setEstilo(est)
+                    listp.push(producto_info)
+                }
+            })
         })
+        setProductos(listp)
     },[])
 
     return(
         <div>
-        {
-            <CardBoletas
-            boleta={recboleta.n_boleta}/>
-        }
+        {recboleta ? (
+            <CardBoletas 
+            brp={recboleta}
+            prods={productos}
+            style={estilo}/>
+        ) : (
+            <p>No se encontró la boleta seleccionada</p>
+        )}
         </div>
     )
 }
