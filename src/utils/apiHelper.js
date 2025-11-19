050a353ffcffff3e1015ecdccac3df50a800e169
BoltlessRobot <luci.escalona@duocuc.cl>
2025-11-19 10:36:27 -0300

    asd

---
+++ src/utils/apiHelper.js
@@ -0,0 +1,37 @@
+const API = 'http://localhost:8094'
+
+export const getDATOS = async () => {
+    try {
+        
+    } catch (ex) {
+        console.error("Error:",ex)
+    }
+}
+export const geDATO = async () => {
+    try {
+        
+    } catch (ex) {
+        console.error("Error:",ex)
+    }
+}
+export const saveDATO = async () => {
+    try {
+        
+    } catch (ex) {
+        console.error("Error:",ex)
+    }
+}
+export const updateDATO = async () => {
+    try {
+        
+    } catch (ex) {
+        console.error("Error:",ex)
+    }
+}
+export const deleteDATO = async () => {
+    try {
+        
+    } catch (ex) {
+        console.error("Error:",ex)
+    }
+}
